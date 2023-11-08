5a8cd2a061444e0f3fc0831d2d9cbbb391494d12
Rahul Narake <narkerahul007@gmail.com>
2023-11-08 10:41:25 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 // require('dotenv').config({ path: './env' });
 import dotenv from 'dotenv';
 import connectToDb from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({ path: './env' });
 
-connectToDb();
+connectToDb()
+  .then(() => {
+    app.on('error', (error) => {
+      console.log('Error :' + error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server started at port :${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log('MONGO DB CONNECTION FAILED !!' + error);
+  });
 
 /*
 const app = express();
